537d7b910630647f459bc44025b9064819b4252a
Andreas Heiduk <asheiduk@gmail.com>
2019-04-23 22:02:34 +0200

    git-feature: delete unnecessary && chains in t0003

--- test/t0003-feature-finish-remote.sh
+++ test/t0003-feature-finish-remote.sh
@@ -4,11 +4,11 @@ load bats-testlib
 
 setup () {
 	say "=================================="
-	private-test-dir &&
-	git init --bare repo-central.git &&
-	git clone --origin origin42 repo-central.git repo-work &&
-	cd repo-work &&
-	setup-default-repo &&
+	private-test-dir
+	git init --bare repo-central.git
+	git clone --origin origin42 repo-central.git repo-work
+	cd repo-work
+	setup-default-repo
 	git push --set-upstream origin42 master develop
 }
 
@@ -20,16 +20,16 @@ setup () {
 
 @test "feature finish with remote branches" {
 	# start feature branch
-	git checkout -b feature/topic1 &&
-	git roboedit -n3 &&
-	git push --set-upstream origin42 feature/topic1 &&
-	verify-graph <<-\EOF &&
+	git checkout -b feature/topic1
+	git roboedit -n3
+	git push --set-upstream origin42 feature/topic1
+	verify-graph <<-\EOF
 		*  (HEAD -> feature/topic1, origin42/feature/topic1)
 		* 
 		* 
 		*  (origin42/master, origin42/develop, master, develop)
 	EOF
-	git feature finish &&
+	git feature finish
 	# implicit checks:
 	#	on develop
 	#	neither local nor remote feature branches
@@ -49,19 +49,19 @@ setup () {
 @test "feature finish while develop ahead upstream" {
 #	skip "value of this test is questionable"
 	# we are ahead of origin42/develop
-	git roboedit -n1 &&
+	git roboedit -n1
 	# now start feature branch
-	git tag develop-old &&
-	git checkout -b feature/topic1 &&
-	git roboedit -n3 &&
-	verify-graph <<-\EOF &&
+	git tag develop-old
+	git checkout -b feature/topic1
+	git roboedit -n3
+	verify-graph <<-\EOF
 		*  (HEAD -> feature/topic1)
 		* 
 		* 
 		*  (tag: develop-old, develop)
 		*  (origin42/master, origin42/develop, master)
 	EOF
-	git feature finish -v &&
+	git feature finish -v
 	# implicit checks:
 	#	on develop
 	#	no "feature/topic1" branch
@@ -81,21 +81,21 @@ setup () {
 
 @test "feature finish while develop behind upstream with fetch" {
 	# we are behind develop@{upstream} (and don't know it!)
-	git roboedit -n1 &&
-	git push &&
-	git reset --hard @~ &&
-	git update-ref refs/remotes/origin42/develop @ &&
+	git roboedit -n1
+	git push
+	git reset --hard @~
+	git update-ref refs/remotes/origin42/develop @
 	# now start feature branch
-	git tag develop-old &&
-	git checkout -b feature/topic1 &&
-	git roboedit -n3 &&
-	verify-graph <<-\EOF &&
+	git tag develop-old
+	git checkout -b feature/topic1
+	git roboedit -n3
+	verify-graph <<-\EOF
 		*  (HEAD -> feature/topic1)
 		* 
 		* 
 		*  (tag: develop-old, origin42/master, origin42/develop, master, develop)
 	EOF
-	git feature finish -v &&
+	git feature finish -v
 	# implicit checks:
 	#	on develop
 	#	develop@{upstream} was updated
@@ -116,18 +116,18 @@ setup () {
 
 @test "feature finish with diverged develop" {
 	# create develop@{upstream}
-	git roboedit -n1 &&
-	git push &&
-	git reset --hard @~ &&
+	git roboedit -n1
+	git push
+	git reset --hard @~
 	# create diverged develop
 	# TODO: this is where git uses "ticks" instead...
-	echo >> develop.txt &&
-	git roboedit -n1 &&
-	git tag develop-old &&
+	echo >> develop.txt
+	git roboedit -n1
+	git tag develop-old
 	# now start feature branch from develop~
-	git checkout -b feature/topic1 @~ &&
-	git roboedit -n3 &&
-	verify-graph <<-\EOF &&
+	git checkout -b feature/topic1 @~
+	git roboedit -n3
+	verify-graph <<-\EOF
 		*  (tag: develop-old, develop)
 		| *  (HEAD -> feature/topic1)
 		| *
@@ -137,7 +137,7 @@ setup () {
 		|/
 		*  (origin42/master, master)
 	EOF
-	run git feature finish -v &&
+	run git feature finish -v
 	[ "$status" = 10 ]
 	# implicit checks:
 	#	nothing changed
