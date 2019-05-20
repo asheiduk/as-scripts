69f6f83200429291fdebc3594dfc89006e66c980
Andreas Heiduk <asheiduk@gmail.com>
2019-05-20 22:09:42 +0200

    git-feature: checks for base-branch and feature-branch

--- test/t0001-feature-start.sh
+++ test/t0001-feature-start.sh
@@ -32,3 +32,28 @@ setup () {
 	EOF
 	assert-config gitflow.branch.feature/topic1.base "develop-gf"
 }
+
+@test "verify base branch checks" {
+	# default is 'develop' -- should work
+	run git feature start topic1
+	(( status == 0 ))
+	git checkout develop
+
+	# rename 'develop' to 'dev' -- should fail
+	git branch -M develop dev
+	run git feature start topic2
+	(( status == 6 ))
+	[ "$output" = "The base branch 'develop' for this feature branch does not exists." ]
+
+	# change/set config -- should work again
+	git config gitflow.branch.develop dev
+	run git feature start topic2
+	(( status == 0 ))
+}
+
+@test "verify feature-branch checks" {
+	git checkout -b feature/topic1
+	run git feature start topic1
+	(( $status == 7 ))
+	[ "$output" = "The feature branch 'feature/topic1' does already exist!" ]
+}
